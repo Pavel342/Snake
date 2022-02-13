0f5c47a6e0b980cbe596b069a660e0217a20956f
Pavel <pavel.samoylo.02@mail.ru>
2022-02-13 12:33:49 +0300

    fire

--- src/game/top/Main.java
+++ src/game/top/Main.java
@@ -11,7 +11,6 @@ public class Main {
         Scanner in=new Scanner(System.in);
         Random rrr=new Random() ;
         boolean eat=false;
-        
         int lenth=1;
         int a=1,b=1;
         int[] masSnakex=new int[100];
@@ -20,7 +19,6 @@ public class Main {
         masSnakey[1]=1;
         boolean gameover=false;
         int fruitx;
-        int ostatokHvosta=0;
         int fruity;
         do{fruitx = 1+(rrr.nextInt(9));
             fruity = 1+(rrr.nextInt(9));}while(fruitx==1 && fruity==1);
@@ -44,7 +42,7 @@ public class Main {
         for(int i=1;i<13;i++){map[11][i]= '-';}
         for(int i=1;i<13;i++){System.out.print(map[11][i]);}
         System.out.println();
-	while (!gameover){
+	while (true){
         String vvod;
 	    do{ vvod= in.next();
 	    if(!vvod.equals("w") & !vvod.equals("s") & !vvod.equals("a") & !vvod.equals("d"))
@@ -54,25 +52,22 @@ public class Main {
 	    int bb=b;
 	    switch (vvod){
             case("w"): if(map[aa-1][b]=='0'){
-                if(ostatokHvosta>=1){ map[aa-1][b]='1';map[ostatokHvosta][b]='0';ostatokHvosta--;}
-                else { map[aa-1][b]='1';
+                map[aa-1][b]='1';
                 int x=masSnakex[lenth];int y=masSnakey[lenth];
                 for(int i=lenth;i>1;i--){masSnakex[i]=masSnakex[i-1];masSnakey[i]=masSnakey[i-1];}
-                masSnakex[1]=aa-1;masSnakey[1]=bb;
-                map[x][y]='0'; }
+                masSnakex[1]=aa-1;masSnakey[1]=b;
+                map[x][y]='0';
                 a--;
             }
             else if(map[a-1][b]=='2'){
                 lenth++;
-                if(ostatokHvosta>=1){ map[aa-1][b]='1';map[ostatokHvosta][b]='0';ostatokHvosta--;}
-                else {map[aa-1][bb]='1';
+                map[aa-1][bb]='1';
                     for(int i=lenth;i>1;i--){masSnakex[i]=masSnakex[i-1];masSnakey[i]=masSnakey[i-1];}
-                    masSnakex[1]=aa-1;masSnakey[1]=bb;}
+                    masSnakex[1]=aa-1;masSnakey[1]=bb;
                 eat=true;
                 a--;
             }
             else if(map[a-1][b]=='-'){
-                if(lenth>1)ostatokHvosta=lenth-1;
                 map[aa+9][b]='1';
                 int x=masSnakex[lenth];int y=masSnakey[lenth];
                 for(int i=lenth;i>1;i--){masSnakex[i]=masSnakex[i-1];masSnakey[i]=masSnakey[i-1];}
@@ -83,25 +78,22 @@ public class Main {
             else if (map[a-1][b]=='1') gameover=true;
                 break;
             case("s"):if(map[aa+1][b]=='0'){
-                if(ostatokHvosta>=1){ map[aa+1][b]='1';map[11-ostatokHvosta][b]='0';ostatokHvosta--;}
-                else {map[aa+1][b]='1';
+
+               map[aa+1][b]='1';
                     int x=masSnakex[lenth];int y=masSnakey[lenth];
                     for(int i=lenth;i>1;i--){masSnakex[i]=masSnakex[i-1];masSnakey[i]=masSnakey[i-1];}
                     masSnakex[1]=aa+1;masSnakey[1]=b;
                     map[x][y]='0';
-                }
                 a++;
             }
             else if(map[aa+1][b]=='2'){
                 lenth++;
-                if(ostatokHvosta>=1){ map[aa-1][b]='1';map[ostatokHvosta][b]='0';ostatokHvosta--;}
-                else {map[aa+1][b]='1';for(int i=lenth;i>1;i--){masSnakex[i]=masSnakex[i-1];masSnakey[i]=masSnakey[i-1];}
-                    masSnakex[1]=aa+1;masSnakey[1]=bb;}
+                map[aa+1][b]='1';for(int i=lenth;i>1;i--){masSnakex[i]=masSnakex[i-1];masSnakey[i]=masSnakey[i-1];}
+                    masSnakex[1]=aa+1;masSnakey[1]=bb;
                 eat=true;
                 a++;
             }
             else if(map[a+1][b]=='-'){
-                if(lenth>1)ostatokHvosta=lenth-1;
                 map[aa-9][b]='1';
                 int x=masSnakex[lenth];int y=masSnakey[lenth];
                 for(int i=lenth;i>1;i--){masSnakex[i]=masSnakex[i-1];masSnakey[i]=masSnakey[i-1];}
@@ -112,26 +104,21 @@ public class Main {
             else if (map[a+1][b]=='1') gameover=true;
                 break;
             case("a"):if(map[aa][b-1]=='0'){
-                if(ostatokHvosta>=1){ map[aa][b-1]='1';map[aa][ostatokHvosta]='0';ostatokHvosta--;}
-                else{map[aa][b-1]='1';
+                map[aa][b-1]='1';
                     int x=masSnakex[lenth];int y=masSnakey[lenth];
                     for(int i=lenth;i>1;i--){masSnakex[i]=masSnakex[i-1];masSnakey[i]=masSnakey[i-1];}
                     masSnakex[1]=aa;masSnakey[1]=b-1;
-                    map[x][y]='0';}
+                    map[x][y]='0';
                 b--;
-
-
             }
             else if(map[a][b-1]=='2'){
                 lenth++;
-                if(ostatokHvosta>=1){ map[aa-1][b]='1';map[aa][ostatokHvosta]='0';ostatokHvosta--;}
-                else{map[aa][b-1]='1';for(int i=lenth;i>1;i--){masSnakex[i]=masSnakex[i-1];masSnakey[i]=masSnakey[i-1];}
-                    masSnakex[1]=aa;masSnakey[1]=bb-1;}
+                map[aa][b-1]='1';for(int i=lenth;i>1;i--){masSnakex[i]=masSnakex[i-1];masSnakey[i]=masSnakey[i-1];}
+                    masSnakex[1]=aa;masSnakey[1]=bb-1;
                 eat=true;
                 b--;
             }
             else if(map[aa][b-1]=='|'){
-                if(lenth>1)ostatokHvosta=lenth-1;
                 map[aa][b+9]='1';
                 int x=masSnakex[lenth];int y=masSnakey[lenth];
                 for(int i=lenth;i>1;i--){masSnakex[i]=masSnakex[i-1];masSnakey[i]=masSnakey[i-1];}
@@ -142,24 +129,21 @@ public class Main {
             else if (map[a][b-1]=='1') gameover=true;
                 break;
             case("d"):if(map[aa][b+1]=='0'){
-                if(ostatokHvosta>=1){ map[aa][b+1]='1';map[aa][11-ostatokHvosta]='0';ostatokHvosta--;}
-                else{map[aa][b+1]='1';
+                map[aa][b+1]='1';
                     int x=masSnakex[lenth];int y=masSnakey[lenth];
                     for(int i=lenth;i>1;i--){masSnakex[i]=masSnakex[i-1];masSnakey[i]=masSnakey[i-1];}
                     masSnakex[1]=aa;masSnakey[1]=b+1;
-                    map[x][y]='0';}
+                    map[x][y]='0';
                 b++;
             }
             else if(map[a][b+1]=='2'){
                 lenth++;
-                if(ostatokHvosta>=1){ map[aa][b+1]='1';map[aa][11-ostatokHvosta]='0';ostatokHvosta--;}
-                else{map[aa][bb+1]='1';for(int i=lenth;i>1;i--){masSnakex[i]=masSnakex[i-1];masSnakey[i]=masSnakey[i-1];}
-                    masSnakex[1]=aa;masSnakey[1]=bb+1;}
+                map[aa][bb+1]='1';for(int i=lenth;i>1;i--){masSnakex[i]=masSnakex[i-1];masSnakey[i]=masSnakey[i-1];}
+                    masSnakex[1]=aa;masSnakey[1]=bb+1;
                 eat=true;
                 b++;
             }
             else if(map[a][b+1]=='|'){
-                if(lenth>1)ostatokHvosta=lenth-1;
                 map[aa][b-9]='1';
                 int x=masSnakex[lenth];int y=masSnakey[lenth];
                 for(int i=lenth;i>1;i--){masSnakex[i]=masSnakex[i-1];masSnakey[i]=masSnakey[i-1];}
