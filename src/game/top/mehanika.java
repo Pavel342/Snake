d73a3fd90a7fc93b45709d775dffd7a289b1ea4c
Pavel <pavel.samoylo.02@mail.ru>
2022-02-14 16:10:18 +0300

    Cool

--- src/game/top/mehanika.java
+++ src/game/top/mehanika.java
@@ -1,7 +1,5 @@
 package game.top;
 
-import java.util.Random;
-import java.util.Scanner;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
@@ -13,17 +11,15 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.util.Random;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public class mehanika {
+public class mehanika extends JPanel implements ActionListener{
 
-    private static final Scanner in = new Scanner(System.in);
-    private static final Random rrr = new Random();
-    private static boolean eat = false;
+    Random r=new Random();
     private static int lenth = 1;
-    private static int a = 1, b = 1;
     private boolean leftDirection = false;
     private boolean rightDirection = true;
     private boolean upDirection = false;
@@ -31,17 +27,18 @@ public class mehanika {
     public static boolean gameover = false;
     private static int fruitx;
     private static int fruity;
-   // private static final char[][] map = new char[13][13];
+    private static final int DELAY=280;
     private static final int shirina=600;
     private static final int visota=500;
     private static final int allDots=3000;
-    private static final int oneDot=20;
+    private static final int oneDot=25;
     private static final int[] masSnakex = new int[allDots];
     private static final int[] masSnakey = new int[allDots];
     private Timer timer;
     private Image telo;
     private Image hui;
     private Image head1;
+
     public mehanika() {
         initmehanika();
     }
@@ -49,257 +46,175 @@ public class mehanika {
     private void initmehanika() {
 
         addKeyListener(new TAdapter());
-        setBackground(Color.black);
+        setBackground(Color.cyan);
         setFocusable(true);
-
-        setPreferredSize(new Dimension(B_WIDTH, B_HEIGHT));
+        setPreferredSize(new Dimension(shirina, visota));
         loadImages();
         initGame();
     }
     private void loadImages() {
 
-        ImageIcon iid = new ImageIcon("src/resurse/telo.png");
+        ImageIcon iid = new ImageIcon("src/resurse/grud.png");
         telo = iid.getImage();
 
-        ImageIcon iia = new ImageIcon("src/resurse/hui.png");
+        ImageIcon iia = new ImageIcon("src/resurse/chlen.png");
         hui = iia.getImage();
 
-        ImageIcon iih = new ImageIcon("src/resurse/head1.png");
+        ImageIcon iih = new ImageIcon("src/resurse/dima.png");
         head1 = iih.getImage();
     }
 
-
-    public static void mehanika(String[] args) {
+    private void initGame() {
         masSnakex[1] = 1;
         masSnakey[1] = 1;
-        do {
-            fruitx = 1 + (rrr.nextInt(9));
-            fruity = 1 + (rrr.nextInt(9));
-        } while (fruitx == 1 && fruity == 1);
+        locateFruit();
+        timer = new Timer(DELAY, this);
+        timer.start();
+    }
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        doDrawing(g);
+    }
+    private void doDrawing(Graphics g) {
 
-        System.out.print("\t\t\t");
-        for (int i = 1; i < 13; i++) {
-            map[0][i] = '-';
-        }
-        for (int i = 1; i < 13; i++) {
-            System.out.print(map[0][i]);
-        }
-        System.out.println();
-        for (int i = 1; i < 11; i++) {
-            System.out.print("\t\t\t");
-            for (int j = 1; j < 11; j++) {
-                if (j == 1) {
-                    map[i][j - 1] = '|';
-                    System.out.print(map[i][j - 1]);
-                }
-                if (fruitx == i && fruity == j) System.out.print(map[i][j] = '2');
-                else if (i == 1 && j == 1) System.out.print(map[i][j] = '1');
-                else System.out.print(map[i][j] = '0');
-                if (j == 10) {
-                    map[i][j + 1] = '|';
-                    System.out.print(map[i][j + 1]);
+        if (!gameover) {
+
+            g.drawImage(hui, fruitx, fruity, this);
+
+            for (int i = 0; i < lenth; i++) {
+                if (i == 0) {
+                    g.drawImage(head1, masSnakex[i], masSnakey[i],this);
+                } else {
+                    g.drawImage(telo, masSnakex[i], masSnakey[i],this);
                 }
             }
-            System.out.println();
-        }
-        System.out.print("\t\t\t");
-        for (int i = 1; i < 13; i++) {
-            map[11][i] = '-';
-        }
-        for (int i = 1; i < 13; i++) {
-            System.out.print(map[11][i]);
-        }
-        System.out.println();
-        while (true) {
-            String vvod;
-            do {
-                vvod = in.next();
-                if (!vvod.equals("w") & !vvod.equals("s") & !vvod.equals("a") & !vvod.equals("d"))
-                    System.out.println("Ты че ввел еблан?( w, a, s, d!!!) ");
-            }
-            while (!vvod.equals("w") & !vvod.equals("s") & !vvod.equals("a") & !vvod.equals("d"));
-            int aa = a;
-            int bb = b;
-            switch (vvod) {
-                case ("w"):
-                    if (map[aa - 1][b] == '0') {
-                        map[aa - 1][b] = '1';
-                        int x = masSnakex[lenth];
-                        int y = masSnakey[lenth];
-                        for (int i = lenth; i > 1; i--) {
-                            masSnakex[i] = masSnakex[i - 1];
-                            masSnakey[i] = masSnakey[i - 1];
-                        }
-                        masSnakex[1] = aa - 1;
-                        masSnakey[1] = b;
-                        map[x][y] = '0';
-                        a--;
-                    } else if (map[a - 1][b] == '2') {
-                        lenth++;
-                        map[aa - 1][bb] = '1';
-                        for (int i = lenth; i > 1; i--) {
-                            masSnakex[i] = masSnakex[i - 1];
-                            masSnakey[i] = masSnakey[i - 1];
-                        }
-                        masSnakex[1] = aa - 1;
-                        masSnakey[1] = bb;
-                        eat = true;
-                        a--;
-                    } else if (map[a - 1][b] == '-') {
-                        map[aa + 9][b] = '1';
-                        int x = masSnakex[lenth];
-                        int y = masSnakey[lenth];
-                        for (int i = lenth; i > 1; i--) {
-                            masSnakex[i] = masSnakex[i - 1];
-                            masSnakey[i] = masSnakey[i - 1];
-                        }
-                        masSnakex[1] = aa + 9;
-                        masSnakey[1] = b;
-                        map[x][y] = '0';
-                        a += 9;
-                    } else if (map[a - 1][b] == '1') gameover = true;
-                    break;
-                case ("s"):
-                    if (map[aa + 1][b] == '0') {
 
-                        map[aa + 1][b] = '1';
-                        int x = masSnakex[lenth];
-                        int y = masSnakey[lenth];
-                        for (int i = lenth; i > 1; i--) {
-                            masSnakex[i] = masSnakex[i - 1];
-                            masSnakey[i] = masSnakey[i - 1];
-                        }
-                        masSnakex[1] = aa + 1;
-                        masSnakey[1] = b;
-                        map[x][y] = '0';
-                        a++;
-                    } else if (map[aa + 1][b] == '2') {
-                        lenth++;
-                        map[aa + 1][b] = '1';
-                        for (int i = lenth; i > 1; i--) {
-                            masSnakex[i] = masSnakex[i - 1];
-                            masSnakey[i] = masSnakey[i - 1];
-                        }
-                        masSnakex[1] = aa + 1;
-                        masSnakey[1] = bb;
-                        eat = true;
-                        a++;
-                    } else if (map[a + 1][b] == '-') {
-                        map[aa - 9][b] = '1';
-                        int x = masSnakex[lenth];
-                        int y = masSnakey[lenth];
-                        for (int i = lenth; i > 1; i--) {
-                            masSnakex[i] = masSnakex[i - 1];
-                            masSnakey[i] = masSnakey[i - 1];
-                        }
-                        masSnakex[1] = aa - 9;
-                        masSnakey[1] = b;
-                        map[x][y] = '0';
-                        a -= 9;
-                    } else if (map[a + 1][b] == '1') gameover = true;
-                    break;
-                case ("a"):
-                    if (map[aa][b - 1] == '0') {
-                        map[aa][b - 1] = '1';
-                        int x = masSnakex[lenth];
-                        int y = masSnakey[lenth];
-                        for (int i = lenth; i > 1; i--) {
-                            masSnakex[i] = masSnakex[i - 1];
-                            masSnakey[i] = masSnakey[i - 1];
-                        }
-                        masSnakex[1] = aa;
-                        masSnakey[1] = b - 1;
-                        map[x][y] = '0';
-                        b--;
-                    } else if (map[a][b - 1] == '2') {
-                        lenth++;
-                        map[aa][b - 1] = '1';
-                        for (int i = lenth; i > 1; i--) {
-                            masSnakex[i] = masSnakex[i - 1];
-                            masSnakey[i] = masSnakey[i - 1];
-                        }
-                        masSnakex[1] = aa;
-                        masSnakey[1] = bb - 1;
-                        eat = true;
-                        b--;
-                    } else if (map[aa][b - 1] == '|') {
-                        map[aa][b + 9] = '1';
-                        int x = masSnakex[lenth];
-                        int y = masSnakey[lenth];
-                        for (int i = lenth; i > 1; i--) {
-                            masSnakex[i] = masSnakex[i - 1];
-                            masSnakey[i] = masSnakey[i - 1];
-                        }
-                        masSnakex[1] = aa;
-                        masSnakey[1] = b + 9;
-                        map[x][y] = '0';
-                        b += 9;
-                    } else if (map[a][b - 1] == '1') gameover = true;
-                    break;
-                case ("d"):
-                    if (map[aa][b + 1] == '0') {
-                        map[aa][b + 1] = '1';
-                        int x = masSnakex[lenth];
-                        int y = masSnakey[lenth];
-                        for (int i = lenth; i > 1; i--) {
-                            masSnakex[i] = masSnakex[i - 1];
-                            masSnakey[i] = masSnakey[i - 1];
-                        }
-                        masSnakex[1] = aa;
-                        masSnakey[1] = b + 1;
-                        map[x][y] = '0';
-                        b++;
-                    } else if (map[a][b + 1] == '2') {
-                        lenth++;
-                        map[aa][bb + 1] = '1';
-                        for (int i = lenth; i > 1; i--) {
-                            masSnakex[i] = masSnakex[i - 1];
-                            masSnakey[i] = masSnakey[i - 1];
-                        }
-                        masSnakex[1] = aa;
-                        masSnakey[1] = bb + 1;
-                        eat = true;
-                        b++;
-                    } else if (map[a][b + 1] == '|') {
-                        map[aa][b - 9] = '1';
-                        int x = masSnakex[lenth];
-                        int y = masSnakey[lenth];
-                        for (int i = lenth; i > 1; i--) {
-                            masSnakex[i] = masSnakex[i - 1];
-                            masSnakey[i] = masSnakey[i - 1];
-                        }
-                        masSnakex[1] = aa;
-                        masSnakey[1] = b - 9;
-                        map[x][y] = '0';
-                        b -= 9;
-                    } else if (map[a][b + 1] == '1') gameover = true;
-                    break;
+            Toolkit.getDefaultToolkit().sync();
+
+        } else {
+            gameOver(g);
+        }
+    }
+    private void gameOver(Graphics g) {
+
+        String msg = "Потрачено...";
+        Font small = new Font("Helvetica", Font.BOLD, 25);
+        FontMetrics metr = getFontMetrics(small);
+        g.setColor(Color.red);
+        g.setFont(small);
+        g.drawString(msg, (shirina - metr.stringWidth(msg)) / 2, visota / 2);
+    }
+    private void checkFruit() {
+
+        if ((masSnakex[0] == fruitx) && (masSnakey[0] == fruity)) {
+
+            lenth++;
+            locateFruit();
+        }
+    }
+    private void move() {
+
+        for (int i = lenth; i > 0; i--) {
+            masSnakex[i] = masSnakex[(i - 1)];
+            masSnakey[i] = masSnakey[(i - 1)];
+        }
+
+        if (leftDirection) {
+            masSnakex[0] -= oneDot;
+        }
+
+        if (rightDirection) {
+            masSnakex[0] += oneDot;
+        }
+
+        if (upDirection) {
+            masSnakey[0] -= oneDot;
+        }
+
+        if (downDirection) {
+            masSnakey[0] += oneDot;
+        }
+    }
+    private void checkCollision() {
+
+        for (int i = lenth; i > 0; i--) {
+
+            if ((i > 4) && (masSnakex[0] == masSnakex[i]) && (masSnakey[0] == masSnakey[i])) {
+                gameover = true;
+                break;
             }
-            repaint();
+        }
+
+        if (masSnakex[0] >= shirina) {
+            masSnakex[0] = 0;
+        }
+
+        if (masSnakex[0] < 0) {
+            masSnakex[0] = shirina;
+        }
+
+        if (masSnakey[0] == visota) {
+            masSnakey[0] = 0;
+        }
+
+        if (masSnakey[0] < 0) {
+            masSnakey[0] = visota;
+        }
+
+        if (gameover) {
+            timer.stop();
         }
     }
 
-    public static void repaint(){
-        if(gameover){System.out.print("Вы проебали:-(");}
-        System.out.print("\t\t\t");
-        for(int i=1;i<13;i++){System.out.print(map[0][i]);}
-        System.out.println();
-        for(int i=1;i<11;i++){
-            System.out.print("\t\t\t");
-            for(int j=1;j<11;j++){
-                if(j==1){        System.out.print('|');
-                }
-                if(eat) {do{fruitx = 1+(rrr.nextInt(9));
-                    fruity = 1+(rrr.nextInt(9));}while(map[fruitx][fruity]!='0');eat=false;}
-                if(fruitx==i && fruity==j ) System.out.print(map[i][j]='2');
-                else System.out.print(map[i][j]);
-                if(j==10){        System.out.print('|');
-                }
-            }
-            System.out.println();
+    private void locateFruit() {
+        fruitx= oneDot* r.nextInt(shirina/oneDot);
+        fruity=oneDot * r.nextInt(visota/oneDot);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        if (!gameover) {
+
+            checkFruit();
+            checkCollision();
+            move();
         }
-        System.out.print("\t\t\t");
-        for(int i=1;i<13;i++){System.out.print(map[11][i]);}
-        System.out.println();
+
+        repaint();
+    }
+
+    private class TAdapter extends KeyAdapter {
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+            int key = e.getKeyCode();
+
+            if ((key == KeyEvent.VK_LEFT || key=='a') && (!rightDirection)) {
+                leftDirection = true;
+                upDirection = false;
+                downDirection = false;
+            }
+
+            if ((key == KeyEvent.VK_RIGHT || key=='d') && (!leftDirection)) {
+                rightDirection = true;
+                upDirection = false;
+                downDirection = false;
+            }
+
+            if ((key == KeyEvent.VK_UP|| key=='w') && (!downDirection)) {
+                upDirection = true;
+                rightDirection = false;
+                leftDirection = false;
+            }
+
+            if ((key == KeyEvent.VK_DOWN || key=='s') && (!upDirection)) {
+                downDirection = true;
+                rightDirection = false;
+                leftDirection = false;
+            }
+        }
     }
 }
